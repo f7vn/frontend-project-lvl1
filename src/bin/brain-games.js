770dd5ea70d80570ee3803736093c59d2e161b2a
f7vn <favnwork@gmail.com>
2019-11-29 13:38:47 +0300

    2 step

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/node
+import sayHello from '..';
+
 console.log("Welcome to the Brain Games");
+sayHello();
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const sayHello = () => {
+const actual = readlineSync.question('Your name is ...');
+console.log(`Hello, ${actual}`);
+return 0;
+}
+
+export default sayHello;
