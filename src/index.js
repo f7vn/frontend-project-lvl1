533ed243a87a96b54045c8d24881d5c81e4bb803
f7vn <favnwork@gmail.com>
2019-11-29 16:31:39 +0300

    EslintV1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const sayHello = () => {
-const actual = readlineSync.question('Your name is ...');
-console.log(`Hello, ${actual}`);
-return 0;
-}
+  const actual = readlineSync.question('Your name is ...');
+  console.log(`Hello, ${actual}`);
+  return 0;
+};
 
 export default sayHello;
